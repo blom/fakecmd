81f0366d9c8bea0220c3615e1661473c21107e6b
Ørjan Blom <blom@blom.tv>
2010-09-27 18:06:56 +0200

    bump version

--- lib/fakecmd/version.rb
+++ lib/fakecmd/version.rb
@@ -2,7 +2,7 @@ module FakeCmd
   module VERSION
     const_defined?(:MAJOR) || MAJOR = 0
     const_defined?(:MINOR) || MINOR = 0
-    const_defined?(:PATCH) || PATCH = 0
+    const_defined?(:PATCH) || PATCH = 1
 
     def self.to_s
       [MAJOR, MINOR, PATCH] * "."
