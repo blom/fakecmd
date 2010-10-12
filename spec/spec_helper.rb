ab86991e2422fe14cd387b01f8659105fb4c1b21
Ørjan Blom <blom@blom.tv>
2010-10-12 18:54:16 +0200

    upgrade to rspec 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 $VERBOSE = nil if RUBY_VERSION["1.8.6"]
 
 require File.expand_path("../../lib/fakecmd", __FILE__)
+require "rspec"
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.before :each do
     FakeCmd.clear!
     FakeCmd.off!
